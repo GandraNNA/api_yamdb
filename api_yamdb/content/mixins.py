b647062b98eb705fac9a0ff802ae2ba10645dd6f
Anna Gandrabura <36998396+GandraNNA@users.noreply.github.com>
2022-07-15 00:15:48 +0300

    Revert "Categories genres titles" (#12)
    Revert "Categories genres titles (#11)"
    
    This reverts commit 94be897809d8fa975a4f2b0a3bc052319540abf9.

--- api_yamdb/content/mixins.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, mixins
-
-class CreateRetrieveDestroyViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet,
-):
-    pass
