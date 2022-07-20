96c99795cb788a20ca3b3caabe466b6caef8376f
Anna Gandrabura <36998396+GandraNNA@users.noreply.github.com>
2022-07-20 03:59:43 +0300

    Final changes (#21)
    * pep8
    
    * pep8
    
    * pep8
    
    * pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -23,5 +23,5 @@ class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-                request.user.is_admin or request.user.is_superuser)
+        return (request.user.is_authenticated and
+                (request.user.is_admin or request.user.is_superuser))
