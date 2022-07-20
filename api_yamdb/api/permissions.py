467536a3dcca5656725504f48597019f96243d9c
GandraNNA <praskovia.gandrabura@yandex.ru>
2022-07-20 17:27:47 +0300

    pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -25,4 +25,3 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated and (
                 request.user.is_admin or request.user.is_superuser))
-                
